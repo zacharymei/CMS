9b5d7ca8eeac7d75da6b7ad56d788ca80aae13c2
JiahengXu <79061449+JiahengXu@users.noreply.github.com>
2021-04-10 00:39:14 -0400

    Added an html page for admins to add student

--- src/main/java/com/comp3004/CMS/controller/AdminController.java
+++ src/main/java/com/comp3004/CMS/controller/AdminController.java
@@ -1,12 +1,26 @@
 package com.comp3004.CMS.controller;
 
 import com.comp3004.CMS.base.Admin;
+import com.comp3004.CMS.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class AdminController {
 
+public class AdminController {
+    @Autowired
+    private AdminService adminService;
+    @GetMapping ("/addStudent")
+    public String addStudent() {
+        if (adminService.loggedIn()){
+            return "redirect:/addStudent.html";
+        }
+        return "redirect:/addStudent.html";
+        //return "Error: You have to be ADMIN to add student.";
+        //Redirect to a page with Error 404 (html)
+    }
 
 
 
